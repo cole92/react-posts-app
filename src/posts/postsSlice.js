59283a22aca222f5ad2dd96ab65b13059f35f603
cole92 <aleksandar9212@gmail.com>
2024-11-17 11:22:28 +0100

    working on postsReducer

--- src/posts/postsSlice.js
+++ src/posts/postsSlice.js
@@ -1,4 +1,13 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+export const getPosts = createAsyncThunk(
+    'posts/getPosts',
+    async () => {
+        return fetch(
+            'https://jsonplaceholder.typicode.com/posts'
+        ).then((response) => response.json())
+    }
+) 
 
 const postSlice = createSlice({
     name: 'posts',
@@ -6,8 +15,18 @@ const postSlice = createSlice({
         list: [], 
         status: null,
     },
-    extraReducers: {
-
+    extraReducers: (builder) => { // Koristimo builder callback notaciju
+        builder
+            .addCase(getPosts.pending, (state) => {
+                state.status = 'loading'; // Kada je poziv u toku
+            })
+            .addCase(getPosts.fulfilled, (state, { payload }) => {
+                state.list = payload; // Kada poziv uspe, azuriraj listu
+                state.status = 'success'; // Postavi status na uspesno
+            })
+            .addCase(getPosts.rejected, (state) => {
+                state.status = 'failed'; // Kada poziv ne uspe
+            });
     },
 });
 
